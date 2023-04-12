c9a958524d527f5bd7d89885da9c01596ce35fca
eraser5th <83107074+eraser5th@users.noreply.github.com>
2023-04-12 14:19:21 +0900

    variables done

--- variables/src/main.rs
+++ variables/src/main.rs
@@ -1,3 +1,21 @@
 fn main() {
-    println!("Hello, world!");
+    let mut x = 5;
+    println!("The value of x is: {}", x);
+    x = 6;
+    println!("The value of x is: {}", x);
+
+    let x = x + 1;
+
+    {
+        let x = x * 2;
+        println!("The value of x in the inner scope is: {}", x);
+    }
+
+    println!("The value of x is: {}", x);
+
+    let spaces = "   ";
+    println!("\"{}\"", spaces);
+
+    let spaces = spaces.len();
+    println!("\"{}\"", spaces);
 }
