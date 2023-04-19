7b413b8843112bd940ca48adbd6e4928a5af8da1
eraser5th <83107074+eraser5th@users.noreply.github.com>
2023-04-19 17:44:33 +0900

    done method

--- ch05/method/src/main.rs
+++ ch05/method/src/main.rs
@@ -8,17 +8,24 @@ impl Rectangle {
     fn area(&self) -> u32 {
         self.width * self.height
     }
+
+    fn square(size: u32) -> Rectangle {
+        Rectangle { width: size, height: size }
+    }
 }
 
 fn main() {
     let rect1 = Rectangle { width: 30, height: 50 };
-
     println!(
         "The area of the rectangle is {} square pixels.",
         rect1.area(),
-    )
+    );
+
+    let rect2 = Rectangle::square(10);
+    println!(
+        "The area of the rectangle is {} square pixels.",
+        rect2.area(),
+    );
+
 }
 
-fn main() {
-    println!("Hello, world!");
-}
